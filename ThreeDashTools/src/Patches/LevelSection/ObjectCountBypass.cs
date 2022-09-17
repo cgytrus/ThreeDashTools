9fa1db052cf38890cb6149027bebf73b0e7b5007
ConfiG <cgytrus@cgyt.ru>
2022-09-17 22:26:59 +0300

    object count bypass

---
+++ ThreeDashTools/src/Patches/LevelSection/ObjectCountBypass.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+
+using SixDash.Patches;
+
+namespace ThreeDashTools.Patches.LevelSection;
+
+[UsedImplicitly]
+public class ObjectCountBypass : ConfigurablePatch {
+    private bool _tempEnabled;
+
+    public ObjectCountBypass() : base(Plugin.instance!.Config, "Level", nameof(ObjectCountBypass), false, "") { }
+
+    public override void Apply() {
+        On.FlatEditor.GetTotalItems += (orig, self) => enabled && _tempEnabled ? 0 : orig(self);
+        On.FlatEditor.UpdateUI += (orig, self) => {
+            orig(self);
+            _tempEnabled = true;
+        };
+        On.FlatEditor.Update += (orig, self) => {
+            orig(self);
+            _tempEnabled = false;
+        };
+    }
+}
